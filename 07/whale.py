430e71db8978238282727ef4ab95bad1ac3aa64c
Bart van der Schans <bart@vanderschans.nl>
2021-12-07 12:21:58 +0100

    Add day 7

---
+++ 07/whale.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import fileinput
+
+from collections import Counter
+
+for line in fileinput.input():
+    P = Counter([int(i) for i in line.strip().split(",")])
+
+
+def fuel(pos):
+    f = 0
+    for p, c in P.items():
+        f += (abs(p-pos)*c)
+    return f
+
+
+def fuel2(pos):
+    f = 0
+    for p, c in P.items():
+        d = (abs(p-pos)) + 1
+        f += int(c * (d * (d - 1) / 2))
+    return f
+
+
+def min(cost_func):
+    prev = None
+    for i in range(max(P.keys())):
+        f = cost_func(i)
+        if prev and f > prev:
+            return prev
+        prev = f
+    return 0
+
+
+print("Fuel part 1:", min(fuel))
+print("Fuel part 2:", min(fuel2))
