4b592c19c7f913132f2d54773605c6f80a26f387
Bart van der Schans <bart@vanderschans.nl>
2021-12-09 16:57:51 +0100

    Cleanup

--- 09/lava.py
+++ 09/lava.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import fileinput
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 M = 0
-L = dict()
+L = defaultdict(int)
 P = list()
 
 for line in fileinput.input():
@@ -13,41 +13,15 @@ for line in fileinput.input():
 
 rows = len(P)
 cols = len(P[0])
-
 for i in range(rows):
     for j in range(cols):
-
-        u = 10
-        d = 10
-        l = 10
-        r = 10
-        if i > 0:
-            u = P[i-1][j]
-        if i < (rows-1):
-            d = P[i+1][j]
-        if j > 0:
-            l = P[i][j-1]
-        if j < (cols - 1):
-            r = P[i][j+1]
-
-        p = P[i][j]
-        if p < u and p < d and p < l and p < r:
-            M += p+1
-            L[(i, j)] = 0
-
-
-for i in range(rows):
-    for j in range(cols):
-        k = i
-        l = j
-        if P[k][l] == 9:
+        if P[i][j] == 9:
             continue
         # walk
+        k = i
+        l = j
+        w = set()
         while True:
-            # check if in min
-            if (k, l) in L:
-                L[(k, l)] += 1
-                break
             # up
             if k > 0 and P[k-1][l] < P[k][l]:
                 k -= 1
@@ -61,7 +35,15 @@ for i in range(rows):
             if l < (cols-1) and P[k][l+1] < P[k][l]:
                 l += 1
 
+            # didn't move
+            if (k, l) in w:
+                L[(k, l)] += 1
+                break
+            else:
+                # add to trail
+                w.add((k, l))
+
 t = [v for k, v in Counter(L).most_common(3)]
 
-print(f"Risk: {M}")
+print(f"Risk: ", sum([P[i][j]+1 for (i, j) in L.keys()]))
 print(f"Basin:", t[0]*t[1]*t[2])
