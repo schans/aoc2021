61e4d06c6c72517514eb33f2c9bf3c88a37723aa
Bart van der Schans <bart@vanderschans.nl>
2021-12-13 11:56:42 +0100

    Cleanup

--- 13/fold.py
+++ 13/fold.py
@@ -11,43 +11,35 @@ for line in fileinput.input():
     cur = line.strip()
     if cur == "":
         continue
-
-    if line.startswith('fold'):
-        (line, nr) = line.split()[-1].split('=')
-        F.append((line, int(nr)))
-    else:
+    if not line.startswith('fold'):
         p = [int(i) for i in line.split(',')]
         P.add((p[0], p[1]))
-
-for (a, n) in F:
-    Q = set()
-
-    for x, y in P:
-        if a == 'y':
-            if y < n:
-                Q.add((x, y))
-            else:
-                Q.add((x, n - (y-n)))
-        elif a == 'x':
-            if x < n:
-                Q.add((x, y))
-            else:
-                Q.add((n - (x-n), y))
-    P = Q
-
-maxx = maxy = 0
-for x, y in P:
-    if x > maxx:
-        maxx = x
-    if y > maxy:
-        maxy = y
+    else:
+        (axis, n) = line.split()[-1].split('=')
+        n = int(n)
+        Q = set()
+        for x, y in P:
+            if axis == 'y':
+                if y < n:
+                    Q.add((x, y))
+                else:
+                    Q.add((x, n - (y-n)))
+            elif axis == 'x':
+                if x < n:
+                    Q.add((x, y))
+                else:
+                    Q.add((n - (x-n), y))
+        P = Q
 
 print(f"Part 1:", len(P))
+
+maxx = max([x for x, _ in P])
+maxy = max([y for _, y in P])
 print("Part 2:")
 for y in range(maxy+1):
     for x in range(maxx+1):
         if (x, y) in P:
             print('#', end="")
         else:
-            print('.', end="")
+            print(' ', end="")
     print()
